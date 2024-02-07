9afc5be93f3855951da1ed01fc0df21b4593cc26
Magesh Kuppan <tkmagesh77@gmail.com>
2024-02-07 15:29:47 +0530

    exercise - solution

--- 02-programming-constructs/06-exercise.go
+++ 02-programming-constructs/06-exercise.go
@@ -21,3 +21,40 @@ Build an interactive calculator
 Assumption:
 	The user will always enter valid data
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var n1, n2, userChoice, result int
+	for {
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Println("5. Exit")
+		fmt.Println("Enter your choice:")
+		fmt.Scanln(&userChoice)
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+		if userChoice == 5 {
+			break
+		}
+		fmt.Println("Enter the operands :")
+		fmt.Scanln(&n1, &n2)
+		switch userChoice {
+		case 1:
+			result = n1 + n2
+		case 2:
+			result = n1 - n2
+		case 3:
+			result = n1 * n2
+		case 4:
+			result = n1 / n2
+		}
+		fmt.Println("Result :", result)
+	}
+}

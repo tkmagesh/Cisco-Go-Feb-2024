522d7d4e3febc28846be35214cedbb35fe9d4e36
Magesh Kuppan <tkmagesh77@gmail.com>
2024-02-07 13:49:40 +0530

    constants

---
+++ 01-intro/04-constants.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// unused constant at package scope (allowed)
+const y = 200
+
+func main() {
+	// const app_version string = "1.0"
+
+	// type inference
+	// const app_version = "1.0"
+
+	const (
+		pi          = 3.14
+		app_version = "1.0"
+	)
+	fmt.Println(pi, app_version)
+
+	// unused constant at function scope (allowed)
+	const x = 100
+}

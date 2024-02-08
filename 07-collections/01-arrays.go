0df18b66e9d19311a76f5c287c77c44751504ebd
Magesh Kuppan <tkmagesh77@gmail.com>
2024-02-08 12:07:23 +0530

    updates - arrays

--- 07-collections/01-arrays.go
+++ 07-collections/01-arrays.go
@@ -40,10 +40,16 @@ func main() {
 	fmt.Println((*nosPtr)[0])
 	fmt.Println(nosPtr[0])
 
-	bubbleSort() //sort the nos array
+	bubbleSort(&nos) //sort the nos array
 	fmt.Println(nos)
 }
 
-func bubbleSort( /* ? */ ) /* no return values */ {
-	/* ? */
+func bubbleSort(nos *[5]int) /* no return values */ {
+	for i := 0; i < 4; i++ {
+		for j := i + 1; j < 5; j++ {
+			if nos[i] > nos[j] {
+				nos[i], nos[j] = nos[j], nos[i]
+			}
+		}
+	}
 }

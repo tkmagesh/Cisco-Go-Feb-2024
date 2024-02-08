cd7e204fa6c8b3064d55aa8cf46e04f73262cfcc
Magesh Kuppan <tkmagesh77@gmail.com>
2024-02-08 14:07:06 +0530

    collections - exercises

---
+++ 07-collections/05-exercise.go
@@ -0,0 +1,4 @@
+/* Write a function "genPrimes" that generates the prime numbers between the given start & end
+
+Use the "genPrimes" function in "main" function to generate the prime numbers between 2 and 100 and print them one after another in the main() function
+*/
